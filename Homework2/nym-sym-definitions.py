11b11050d36afea2850280e994864447c9083ed7
Aadil Tajani <61664466+aadiltajani@users.noreply.github.com>
2022-09-05 17:49:28 -0400

    converted functions to classes for sym and num

--- Homework2/nym-sym-definitions.py
+++ Homework2/nym-sym-definitions.py
@@ -1,10 +1,21 @@
 import sys
 
 
-def sym_new(c:int, s:str):
-    return {'n': 0, 'at': c or 0, 'name': s or '', '_has': {}}
+class Sym():
+    def _init_(self, c: int, s: str):
+        self.n = 0
+        self.at = c or 0
+        self.name = s or ''
+        self._has = {}
 
 
-def num_new(c:int, s:str):
-    return {'n': 0, 'at': c or 0, 'name': s or '', '_has': {}, 'lo': -sys.maxsize - 1, 'high': sys.maxsize,
-            'isSorted': True, 'w': (s or '').find('-$') == -1 and -1 or 1}
+class Num():
+    def _init_(self, c: int, s: str):
+        self.n = 0
+        self.at = c or 0
+        self.name = s or ''
+        self._has = {}
+        self.lo = -sys.maxsize - 1
+        self.high = sys.maxsize
+        self.isSorted = True
+        self.w = (s or '').find('-$') == -1 and -1 or 1
