d0ee9f908058592a1127872b6bfc2f852a4e546f
ArpitCh21 <112001933+ArpitCh21@users.noreply.github.com>
2022-09-05 13:59:35 -0400

    Num functions
    num, add, mid, div and per functions

---
+++ Homework2/num.py
@@ -0,0 +1,45 @@
+import random
+# Num nums
+def nums():
+    if not self.isSorted:
+        t.sort(self._has)
+        self.isSorted = True
+
+    return self._has
+
+
+# Num Add
+def add(v, pos):
+    if type(v) == int or type(v) == float:
+        self.n += 1
+        self.lo = min(v, self.lo)
+        self.hi = max(v, self.hi)
+
+        if self._has < the.nums:
+            pos = 1 + self._has
+        elif random.random() < the.nums/self.n:
+            pos = random.random(self._has)
+
+        if pos:
+            self.isSorted = False
+            self._has[pos] = tonumber(v)
+    else:
+        print("Symbol encountered!!")
+
+
+# Num Div
+def div(a):
+    a = self:nums()
+    return (per(a,0.9) − per(a,0.1))/2.58
+
+
+# Num Mid
+def mid():
+    return per(self:nums(),0.5)
+
+# per
+def per(t, p):
+    p = math.floor(((p or 0.5 )* t) + 0.5)
+    return t[max(1, min(t,p))]
+
+
