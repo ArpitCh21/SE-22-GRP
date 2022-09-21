e0cf7bd2d984b6ee8bd920a2d3baf9170174b934
ArpitCh21 <112001933+ArpitCh21@users.noreply.github.com>
2022-09-21 18:17:06 -0400

    Add files via upload

---
+++ Homework2/csv func.py
@@ -0,0 +1,22 @@
+import csv
+import math
+import io
+class CsvFile:
+
+    def __init__(self, the, t):
+        self.t = the
+
+
+    def csv(self, fname, fun, sep, src, s, t):
+        sep = "([^" ..the.separator.. "]+)"
+        src = input(fname)
+        while True:
+            s = io.read()
+            if not s:
+                return io.close(src)
+            else:
+                t = {}
+                for s1 in s.gmatch(sep):
+                    t[1+t] = coerce(s1)
+
+                fun(t)
