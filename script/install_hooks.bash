3ea3c85e0bde61740dd7085fab5d4bb4622ebbc4
kaustubh <kadeshpa@ncsu.edu>
2022-09-27 11:49:16 -0400

    permission type change

--- script/install_hooks.bash
+++ script/install_hooks.bash
@@ -1,2 +1,2 @@
-#!/bin/sh
-cd .. && cd .git/hooks && ln -s ../../test/run_test_from_hook.bash post-commit && chmod a+x post-commit
+#!/bin/bash
+cd .. && cd .git/hooks && ln -s ../../test/run_test_from_hook.bash post-commit && chmod 755 post-commit
