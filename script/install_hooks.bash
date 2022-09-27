f22516a12c97509c9bd07c3a52971774682afd1e
kaustubh <kadeshpa@ncsu.edu>
2022-09-27 11:14:57 -0400

    giving permission to post commit to execute

---
+++ script/install_hooks.bash
@@ -0,0 +1,2 @@
+#!/bin/sh
+cd .. && cd .git/hooks && ln -s ../../test/run_test_from_hook.bash post-commit && chmod 755 post-commit
--- script/run-tests.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd ..
-cd  test
-python test.py
