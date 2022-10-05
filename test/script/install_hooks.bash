9d705d304ef7982ed5c5a8efce9c49fbe7a0033f
kaustubh <kadeshpa@ncsu.edu>
2022-10-05 15:18:56 -0400

    changing install_hooks script to accomodate for repo reorg

--- test/script/install_hooks.bash
+++ test/script/install_hooks.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd .. && cd .git/hooks && ln -s ../../test/run_test_from_hook.bash post-commit && chmod 755 post-commit
+cd .. && cd .. && cd .git/hooks && ln -s ../../test/run_test_from_hook.bash post-commit && chmod 755 post-commit
