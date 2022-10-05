8bc97f985742ea0728de56b86b13b3863d0b94b6
Dhruvish Patel <53443266+Dhruvish-Patel@users.noreply.github.com>
2022-10-05 13:00:49 -0400

    Update test_cases.py

--- test/test_cases.py
+++ test/test_cases.py
@@ -42,59 +42,59 @@ def test_all():
 
 
 
-def runs(k):
-    if (not eg.get(k)):
-        return
+# def runs(k):
+#     if (not eg.get(k)):
+#         return
 
-    # the['seed'] = random.seed(10)
-    random.seed(10)
-    for k, v in the.items():
-        old[k] = the[k]
+#     # the['seed'] = random.seed(10)
+#     random.seed(10)
+#     for k, v in the.items():
+#         old[k] = the[k]
 
-    if ('dump' in the.keys()):
-        status = True
-        out = eg[k]
-    # Else statement is remaining
-    else:
-        status = False
-        out = eg[k]
-    for k in old:
-        the[k] = old[k]
+#     if ('dump' in the.keys()):
+#         status = True
+#         out = eg[k]
+#     # Else statement is remaining
+#     else:
+#         status = False
+#         out = eg[k]
+#     for k in old:
+#         the[k] = old[k]
 
-    msg = status and ((out == True and "PASS") or "FAIL") or "CRASH"
-    print("!!!!!!", msg, k, status)
-    return out
-
-
-def BAD():
-    print('eg dont have this field')
+#     msg = status and ((out == True and "PASS") or "FAIL") or "CRASH"
+#     print("!!!!!!", msg, k, status)
+#     return out
 
 
-def LIST():
-    t = {}
-    for k in eg:
-        t[1 + len(t)] = k
-    t = OrderedDict(sorted(t.items()))
-    return t
+# def BAD():
+#     print('eg dont have this field')
 
 
-def LS():
-    print("\nExamples lua csv −e ...")
-    lst = LIST()
-    for _, k in lst.items():
-        print("\t", k)
-    return True
+# def LIST():
+#     t = {}
+#     for k in eg:
+#         t[1 + len(t)] = k
+#     t = OrderedDict(sorted(t.items()))
+#     return t
 
 
-def ALL():
-    fails = 0
-    lst = LIST()
-    for _, k in lst.items():
-        if (k != "ALL"):
-            print("\n−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−")
-            if not runs(k):
-                fails = fails + 1
-    return True
+# def LS():
+#     print("\nExamples lua csv −e ...")
+#     lst = LIST()
+#     for _, k in lst.items():
+#         print("\t", k)
+#     return True
+
+
+# def ALL():
+#     fails = 0
+#     lst = LIST()
+#     for _, k in lst.items():
+#         if (k != "ALL"):
+#             print("\n−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−")
+#             if not runs(k):
+#                 fails = fails + 1
+#     return True
 
 
 # def the():
